e15d3a20ddec65bd9cdb84b0a31cf626c7ff02d1
rn4nv <dorjuan@ya.ru>
2022-12-11 01:20:15 +0700

    Completed CS task 001

---
+++ CS_task_001/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.Write("Введите число 'A':");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральное число 'B':");
+int b = int.Parse(Console.ReadLine());
+int i = 1;
+int result = 1;
+
+if (b > 0)
+{
+    while (i <= b)
+    {
+        result = result * a;
+        i++;
+    }
+    Console.Write("Результат возведения числа 'A', в степень равную числу 'B' равняется: ");
+    Console.Write(result);
+}
+else
+{
+    Console.WriteLine("Введенное число 'B' не является натуральным. Запустите программу заново, введя новые значения.");
+}
