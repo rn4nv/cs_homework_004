71889fa1135a42b01998ade65ce418a4fad8b6ab
rn4nv <dorjuan@ya.ru>
2022-12-11 20:29:31 +0700

    Completed CS task 001 version 2.0

--- CS_task_001/Program.cs
+++ CS_task_001/Program.cs
@@ -1,23 +1,53 @@
 ﻿Console.Clear();
 
+int Squaring (int A, int B)
+{
+    int n = 1;
+    int result = 1;
+    if (B > 0)
+    {
+        while (n <= B)
+        {
+            result = result * A;
+            n++;
+        }
+        Console.Write("Результат возведения числа 'A', в степень равную числу 'B' равняется: ");
+        Console.Write(result);
+    }
+    else
+    {
+        Console.WriteLine("Введенное число 'B' не является натуральным. Запустите программу заново, введя новые значения.");
+    }
+    return result;
+}
+
 Console.Write("Введите число 'A':");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите натуральное число 'B':");
 int b = int.Parse(Console.ReadLine());
-int i = 1;
-int result = 1;
 
-if (b > 0)
-{
-    while (i <= b)
-    {
-        result = result * a;
-        i++;
-    }
-    Console.Write("Результат возведения числа 'A', в степень равную числу 'B' равняется: ");
-    Console.Write(result);
-}
-else
-{
-    Console.WriteLine("Введенное число 'B' не является натуральным. Запустите программу заново, введя новые значения.");
-}
+Squaring(a, b);
+
+// Вариант решения без использования функций.
+
+// Console.Write("Введите число 'A':");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите натуральное число 'B':");
+// int b = int.Parse(Console.ReadLine());
+// int i = 1;
+// int result = 1;
+
+// if (b > 0)
+// {
+//     while (i <= b)
+//     {
+//         result = result * a;
+//         i++;
+//     }
+//     Console.Write("Результат возведения числа 'A', в степень равную числу 'B' равняется: ");
+//     Console.Write(result);
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число 'B' не является натуральным. Запустите программу заново, введя новые значения.");
+// }
